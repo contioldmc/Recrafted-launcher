d2786448cccbe2c22b9598c5e48e3f0f2f07e2f0
contioldmc <contioldmc@gmail.com>
2025-05-01 22:08:47 +0200

    opensource recrafted
    this is the opensource file. open it in visual studio (if it doesnt work then im sorry, im new to programing and github)

---
+++ recrafted/Minecraft Launcher/Minecraft Launcher/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Minecraft_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(181, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(506, 61);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Minecraft Launcher";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(377, 218);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(269, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(29, 177);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(107, 20);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Got Problem?";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(138, 112);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(186, 24);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Remember password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(138, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(186, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Username:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Location = new System.Drawing.Point(241, 137);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(384, 224);
+            this.panel2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Minecraft_Launcher.Properties.Resources._427px_Dirt_background_BE2;
+            this.ClientSize = new System.Drawing.Size(843, 466);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Recrafted! v1.0  Alpha 1.1.2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ recrafted/Minecraft Launcher/Minecraft Launcher/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minecraft_Launcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+            panel1.BorderStyle = BorderStyle.FixedSingle; // this should fix the boarder style
+            // plot twist, it actually did not worked, i had to create the boarder style myself trough panel
+            
+           
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://mail.google.com/mail/?view=cm&fs=1&to=contioldmc@gmail.com");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // checks if the account and password are filled
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                // if some details arent filled in
+                MessageBox.Show("Please fill in all details. (Account and Password).", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                string username = textBox1.Text;
+                string password = textBox2.Text;
+
+                // Verify that the username and password are correct
+                if ((username == "cowweb" || username == "puppiu") && password == "goldenager")
+                {
+                    // this gets the path to the folder where the EXE is (IMPORTANT)
+                    string basePath = AppDomain.CurrentDomain.BaseDirectory;
+
+                    // Relative paths to folders and files
+                    string minecraftJar = Path.Combine(basePath, "jars", "b1.2.jar");
+                    string librariesPath = Path.Combine(basePath, "libraries");
+                    string nativesPath = Path.Combine(basePath, "natives");
+
+                    try
+                    {
+                        // Setting the classpath to run Minecraft (IMPORTANT)
+                        string classpath =
+                            $"\"{minecraftJar}\";" +
+                            $"\"{Path.Combine(librariesPath, "lwjgl.jar")}\";" +
+                            $"\"{Path.Combine(librariesPath, "lwjgl_util.jar")}\";" +
+                            $"\"{Path.Combine(librariesPath, "jinput.jar")}\"";
+
+                        // Starting a Minecraft process (IMPORTANT)
+                        var startInfo = new System.Diagnostics.ProcessStartInfo
+                        {
+                            FileName = "java",
+                            Arguments = $"-Xmx2G -Xms1G -Djava.library.path=\"{nativesPath}\" -cp {classpath} net.minecraft.client.Minecraft {username}",
+                            WorkingDirectory = Path.GetDirectoryName(minecraftJar),
+                            UseShellExecute = false,
+                            CreateNoWindow = true
+                        };
+
+                        System.Diagnostics.Process.Start(startInfo);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error trying to run the game: {ex.Message}", "Mistakes", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    // If the user enters the wrong name or password
+                    MessageBox.Show("Invalid username or password. Please try again.", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+        // dont know why that is here but if i delete it, the code just stops working,,
+
+
+        private void versionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    }
+
+    
+    
+    
+    
+
